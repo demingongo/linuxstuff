8e811ee9545992736f0f5619fde31fe6079edffd
demingongo <sdemingongo@gmail.com>
2023-01-14 04:12:34 +0100

    fix mkdir => mkdir -p + instructions

--- arch_install_grub.sh
+++ arch_install_grub.sh
@@ -26,7 +26,7 @@ fi
 
 ## Install grub and stuff
 sudo pacman -S grub efibootmgr dosfstools os-prober mtools
-sudo mkdir /efi
+sudo mkdir -p /efi
 sudo mount /dev/sda1 /efi
 DFLT_BOOTLOADER_ID='arch_linux'
 echo
@@ -34,7 +34,7 @@ echo -n "Enter bootloader id (default: $DFLT_BOOTLOADER_ID) #: "
 read BOOTLOADER_ID
 BOOTLOADER_ID="${BOOTLOADER_ID:-$DFLT_BOOTLOADER_ID}"
 sudo grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id="$BOOTLOADER_ID"
-sudo mkdir /boot/grub/locale
+sudo mkdir -p /boot/grub/locale
 sudo cp /usr/share/locale/en\@quot/LC_MESSAGES/grub.mo /boot/grub/locale/en.mo
 sudo sed -i '/^GRUB_DISABLE_OS_PROBER=/d' /etc/default/grub
 echo "GRUB_DISABLE_OS_PROBER=false" | sudo tee -a /etc/default/grub
@@ -45,9 +45,12 @@ echo
 echo -e "\033[0;32mGREAT ...\033[0m"
 echo -e "\033[0;33mBUT YOU STILL HAVE SOMETHING TO DO! ===> FOLLOW THE INSTRUCTIONS :\033[0m"
 echo
-echo "   1. Mount the partitions from which the other systems boot (e.g.: mkdir /mnt/fedora && mount /mnt/sxyz /mnt/fedora)"
-echo "      (The exact mount point does not matter)"
+echo "   1. Mount the partitions from which the other systems boot."
+echo "      This is optional. The exact mount point does not matter."
+echo "      (e.g.: mkdir /mnt/fedora && mount /mnt/sxyz /mnt/fedora)"
 echo
 echo "   2. Run grub-mkconfig :"
 echo "      grub-mkconfig -o /boot/grub/grub.cfg"
+echo
+echo "   3. Unmount what you mounted from instruction 1."
 echo
