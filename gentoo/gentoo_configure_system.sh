2402b7534ec1b2b943e109c130894e44e42ef8b4
demingongo <sdemingongo@gmail.com>
2023-11-25 04:15:05 +0100

    gentoo package.use

--- gentoo/gentoo_configure_system.sh
+++ gentoo/gentoo_configure_system.sh
@@ -43,7 +43,7 @@ else
     # Add local flags or networkmanager
     echo """
 net-misc/networkmanager iwd
-""" >> /etc/portage/package.use/02network
+""" >> /etc/portage/package.use/network
     # Install packages
     emerge --ask --newuse net-misc/networkmanager net-wireless/iwd net-wireless/iw
     echo
